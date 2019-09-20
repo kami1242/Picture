625a76afa608b21efe4e81ae2b21e517523bd44a
Paul <arkhipov.pa@phystech.edu>
2019-09-20 15:47:53 +0300

    Test task

---
+++ lab3/smile.py
@@ -0,0 +1,23 @@
+from graph import *
+
+windowSize(400, 400)
+
+penColor("black")
+penSize(1)
+brushColor("yellow")
+circle(200, 200, 100)
+
+brushColor("red")
+circle(250, 180, 20)
+circle(150, 180, 20)
+brushColor("black")
+circle(250, 180, 10)
+circle(150, 180, 10)
+
+rectangle(150, 250, 250, 270)
+
+penSize(10)
+line(180, 170, 100, 120)
+line(220, 170, 300, 120)
+
+run()
