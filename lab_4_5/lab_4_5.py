1ed8c8449b0c4ceb30f10f42bd1e0d52dadf5c1d
Paul <arkhipov.pa@phystech.edu>
2019-10-18 18:00:38 +0300

    Rectangles are added

--- lab_4_5/lab_4_5.py
+++ lab_4_5/lab_4_5.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from random import randrange as rnd, choice
+import math
 import time
 
 root = Tk()
@@ -9,6 +10,7 @@ canv.pack(fill=BOTH,expand=1)
 points = 0 #user's points
 n = 5 #number of the balls
 balls = []
+rects = []
 
 class Ball:
     def __init__(self):
@@ -17,7 +19,7 @@ class Ball:
         self.r = rnd(30, 50)
         self.vx = rnd(10) - 5
         self.vy = rnd(10) - 5
-        self.color = choice(['red','orange','yellow','green','blue'])
+        self.color = choice(['red', 'orange', 'yellow', 'green', 'blue'])
         self.circle = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill = self.color, width=0)
     def update(self):
         global canv
@@ -27,29 +29,67 @@ class Ball:
             self.vx = abs(self.vx)
         if self.y > 500:
             self.vy = -abs(self.vy)
-        if self.x < 100:
+        if self.y < 100:
             self.vy = abs(self.vy)
         self.x += self.vx
         self.y += self.vy
         canv.move(self.circle, self.vx, self.vy)
 
-def dist(x1, y1, z1, x2): #returns distance between two points
-    return sqrt((x1 - x2)**2 + (y1 - y2)**2)
+class Rect:
+    def __init__(self):
+        self.x = rnd(100, 700)
+        self.y = rnd(100, 500)
+        self.a = rnd(30, 50)
+        self.vx = rnd(30) - 15
+        self.vy = rnd(30) - 15
+        self.rect = canv.create_rectangle(self.x, self.y, self.x + self.a, self.y + self.a, fill = 'black')
+    def update(self):
+        global canv
+        self.vx += rnd(30) - 15 - (self.x - 400)*abs((self.x - 400))*0.0002 - 0.2*self.vx
+        self.vy += rnd(30) - 15 - (self.y - 300)*abs((self.y - 300))*0.0002 - 0.2*self.vy
+        self.x += self.vx
+        self.y += self.vy
+        canv.move(self.rect, self.vx, self.vy)
+
+def dist(x1, y1, x2, y2): #returns distance between two points
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
 def new_ball():
     balls.append(Ball())
-    #canv.create_oval(x-r, y-r, x+r, y+r, fill = choice(colors), width=0)
+
+def new_rect():
+    rects.append(Rect())
 
 def update():
     for b in balls:
         b.update()
+    for r in rects:
+        r.update()
     root.after(100, update)
 
 def click(event):
-    print()
+    global points
+    global canv
+    for b in balls:
+        if dist(event.x, event.y, b.x, b.y) < b.r:
+            points += 1
+            canv.delete(b.circle)
+            balls.remove(b)
+            new_ball()
+    for r in rects:
+        if (event.x > r.x) and (event.x < r.x + r.a) and (event.y > r.y) and (event.y < r.y + r.a):
+            points += 5
+            canv.delete(r.rect)
+            rects.remove(r)
+            new_rect()
+    print(points)
 
 new_ball()
 new_ball()
+new_rect()
+new_rect()
+print(rects)
+    
 update()
 canv.bind('<Button-1>', click)
 
